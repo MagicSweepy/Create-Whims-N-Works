5814e7633ebe9553a65c5b248b1280d0c4ac024c
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-05-25 10:34:53 +0800

    Initial Commit.

---
+++ src/main/scala/magicbook/whimsnworks/WhimsNWorks.scala
@@ -0,0 +1,13 @@
+package magicbook.whimsnworks
+
+import net.neoforged.fml.common.Mod
+
+trait WhimsNWorks { // TODO: NF life cycle for mod impl?
+}
+
+@Mod(WhimsNWorks.ID)
+object WhimsNWorks {
+
+    final val ID = "whimsnworks"
+
+}
